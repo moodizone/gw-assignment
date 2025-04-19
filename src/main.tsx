c2a989a30f665d643cd4710fdfa4f49276bd2a8b
moodi <moodizone@gmail.com>
2025-04-20 00:20:24 +0330

    🚧 register qclient

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "./index.css";
 import Router from "./router/index.tsx";
+
+const queryClient = new QueryClient();
 
 async function enableMocking() {
   const { worker } = await import("./mock");
@@ -17,7 +20,9 @@ async function enableMocking() {
 enableMocking().then(() => {
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
-      <Router/>
+      <QueryClientProvider client={queryClient}>
+        <Router />
+      </QueryClientProvider>
     </StrictMode>
   );
 });
