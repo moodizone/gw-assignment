eb049f44957850bf398da75ba287bebfa4c14b3c
moodi <moodizone@gmail.com>
2025-04-19 20:14:25 +0330

    🚧 add UI components for building web pages

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,15 @@
 import * as React from "react";
 
 function Layout({ children }: React.PropsWithChildren) {
-  return <main>{children}</main>;
+  return (
+    <div>
+      <header className="flex sticky top-0 bg-background h-16 shrink-0 items-center gap-2 border-b px-4">
+        <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+      </header>
+      {children}
+    </div>
+  );
 }
 
 export default Layout;
