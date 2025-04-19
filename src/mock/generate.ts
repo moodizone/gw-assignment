ec554cd3e7c46ae197c6aeb9fb033bd4101a0d12
moodi <moodizone@gmail.com>
2025-04-19 18:53:55 +0330

    🚨 lint

--- src/mock/generate.ts
+++ src/mock/generate.ts
@@ -1,6 +1,7 @@
 import { Item } from "@/services/type";
 
-// @ts-expect-error package discontinued
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore package discontinued
 import { fa, Faker } from "@faker-js/faker";
 
 export const faker = new Faker({
@@ -9,16 +10,16 @@ export const faker = new Faker({
 
 export function generateItems(count = 10_000): Item[] {
   const categories = ["Electronics", "Books", "Clothing", "Sports", "Home"];
-return Array.from({ length: count }, (_, i) => ({
+  return Array.from({ length: count }, (_, i) => ({
     id: i + 1,
     title: faker.commerce.productName(),
     category: faker.helpers.arrayElement(categories),
     date: faker.date
-        .between({ from: "2022-01-01", to: "2024-12-31" })
-        .toISOString(),
+      .between({ from: "2022-01-01", to: "2024-12-31" })
+      .toISOString(),
     price: parseFloat(faker.commerce.price({ min: 10, max: 1000 })),
     description: faker.lorem.sentence(),
     stock: faker.number.int({ min: 0, max: 500 }),
-    rating: parseFloat(faker.number.float({ min: 1, max: 5, precision: 0.1 }).toFixed(1)),
-}));
+    rating: parseFloat(faker.number.float({ min: 1, max: 5 }).toFixed(1)),
+  }));
 }
