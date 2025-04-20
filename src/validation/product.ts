c47203f70cbb639faffe445473bfc4767aff30c5
moodi <moodizone@gmail.com>
2025-04-20 15:14:09 +0330

    🦺 add product schema

---
+++ src/validation/product.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const productSchema = z.object({
+  title: z.string().min(3, "Title is required"),
+  category: z.string().min(1, "Category is required"),
+  description: z.string().optional(),
+  price: z.number().positive("Price must be a positive number"),
+  stock: z.number().int().positive("Stock must be a positive integer"),
+});
